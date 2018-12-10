921f75ff7079c371f2bc09a7afedc6e00998dd60
Wojciech Nizialek <wojciech.nizialek@gmail.com>
2018-12-10 14:50:23 +0000

    Add prefix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
+
 function getQuote() {
-    fetch(quoteUrl, { cache: "no-store" })
+    fetch(prefix + quoteUrl, { cache: "no-store" })
         .then(function(resp) {
             return resp.json();
         })
